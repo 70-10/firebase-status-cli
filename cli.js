993b9812c7c9af6b342e57da9579303c6a7ee9e2
70_10 <7010.dev@gmail.com>
2017-09-30 23:51:04 +0900

    [mod] del cheerio-httpcli

--- cli.js
+++ cli.js
@@ -2,16 +2,14 @@
 
 const chalk = require("chalk");
 const firebaseStatus = require(".");
+firebaseStatus().then(services => {
+  if (services.length <= 0) {
+    console.log(chalk.green.bold("All services are available."));
+    return;
+  }
 
-const services = firebaseStatus();
-
-if (services.length <= 0) {
-  console.log(chalk.green.bold("All services are available."));
-  return;
-}
-
-
-Object.keys(services).forEach(serviceName => {
-  console.log(chalk.bold.underline.red(serviceName));
-  services[serviceName].forEach(i => console.log(i));
+  Object.keys(services).forEach(serviceName => {
+    console.log(chalk.bold.underline.red(serviceName));
+    services[serviceName].forEach(i => console.log(i));
+  });
 });
