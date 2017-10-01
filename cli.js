0558e7f5d69ce3ef74f7353c91c4c1047db94b0d
70_10 <7010.dev@gmail.com>
2017-10-01 23:53:58 +0900

    [mod] cli error handling

--- cli.js
+++ cli.js
@@ -2,14 +2,17 @@
 
 const chalk = require("chalk");
 const firebaseStatus = require(".");
-firebaseStatus().then(services => {
-  if (services.length <= 0) {
-    console.log(chalk.green.bold("All services are available."));
-    return;
-  }
 
-  Object.keys(services).forEach(serviceName => {
-    console.log(chalk.bold.underline.red(serviceName));
-    services[serviceName].forEach(i => console.log(i));
-  });
-});
+firebaseStatus()
+  .then(services => {
+    if (services.length <= 0) {
+      console.log(chalk.green.bold("All services are available."));
+      return;
+    }
+
+    Object.keys(services).forEach(serviceName => {
+      console.log(chalk.bold.underline.red(serviceName));
+      services[serviceName].forEach(i => console.log(i));
+    });
+  })
+  .catch(console.error);
